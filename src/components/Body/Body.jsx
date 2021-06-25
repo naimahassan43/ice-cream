d42087f4ae288256d506d72a966f361f75c82ab0
Syeda Naima Hassan <naima.hassan434@gmail.com>
2021-06-25 02:21:21 +0100

    Layout component and got a structure

---
+++ src/components/Body/Body.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Body.module.css';
+
+function Body() {
+  return (
+    <div className={classes.mainBody}>
+
+    </div>
+  )
+}
+
+export default Body
