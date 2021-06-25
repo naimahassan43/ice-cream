5cd1c50aae5c9c4dd74747fe6f77e597ffb28fc8
Syeda Naima Hassan <naima.hassan434@gmail.com>
2021-06-25 01:53:15 +0100

    Footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import classes from './Footer.module.css';
+
+function Footer() {
+ return (
+  <footer>
+  <div className={['container', classes.container].join(' ')}>
+    <div>
+      Copyright &copy;2020.
+    </div>
+    <div className="textRight">
+      Built with <span class="red">&hearts;</span> by
+      <a
+        href="https://reactjs.org/docs/create-a-new-react-app.html" >Create React App
+        
+        </a >
+    </div>
+  </div>
+</footer>
+ )
+}
+
+export default Footer
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from '../assets/images/logo.svg';
-import reactLogo from '../assets/images/react.svg';
+import logo from '../../assets/images/logo.svg';
+import reactLogo from '../../assets/images/react.svg';
 import classes from './Header.module.css';
 
 const Header = () => {
